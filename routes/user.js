62d90bdcdc56fd24e5706f2f05f62ee50c193709
samuelyuoh <samuelyuoh@gmail.com>
2022-06-22 23:10:48 +0800

    added profile page and editing info

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,7 @@ const flashMessage = require('../helpers/messenger');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
-
-router.get('/', (req, res) => {
-    res.render('user/login');
-});
+const ensureAuthenticated = require('../helpers/auth');
 
 router.get('/login', (req, res) => {
     res.render('user/login');
@@ -77,6 +74,45 @@ router.post('/login', (req, res, next) => {
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
+});
+
+router.get('/profile/:id', ensureAuthenticated, (req, res) => {
+    User.findByPk(req.params.id)
+        .then((user) => {
+            if (!user) {
+                flashMessage(res, 'error', 'User not found');
+                res.redirect('/user/login');
+                return;
+            }
+            // if (req.user.id != req.params.id) {
+            //     flashMessage(res, 'error', 'Unauthorised access');
+            //     res.redirect('/');
+            //     return;
+            // }
+            res.render('user/profile', { user });
+            
+        })
+        .catch(err => console.log(err));
+});
+
+router.post('/profile/:id', ensureAuthenticated, (req, res) => {
+    let name = req.body.name ? req.body.name : null;
+    let email = req.body.email ? req.body.email : null;
+    let phoneNumber = req.body.phoneNumber ? req.body.phoneNumber : null;
+    let address = req.body.address ? req.body.address : null;
+    let address2 = req.body.address2 ? req.body.address2 : null;
+    let postalCode = req.body.postalCode ? req.body.postalCode : null;
+    let password = req.body.password ? req.body.password : null;
+    // FHHA9XFM.isd/Uqx1tTMOkrcQC68H5ce2o2W8mYa
+    User.update(
+        { name, email, password, phoneNumber, address, address2, postalCode }, 
+        { where: { id: req.params.id } }
+    )
+        .then((result) => {
+            flashMessage(res, 'success', 'Profile updated');
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
 });
 
 module.exports = router;
