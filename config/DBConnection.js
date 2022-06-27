7347cb699fc7f53651cd5232caf8bb7895920e15
Kitsunez <95900200+Kitsunez@users.noreply.github.com>
2022-06-27 10:14:22 +0800

    updated dbconnection

--- config/DBConnection.js
+++ config/DBConnection.js
@@ -1,6 +1,5 @@
 const mySQLDB = require('./DBConfig');
 const User = require('../models/User');
-const Video = require('../models/Video');
 
 // If drop is true, all existing tables are dropped and recreated
 const setUpDB = (drop) => {
@@ -11,8 +10,6 @@ const setUpDB = (drop) => {
             Defines the relationship where a user has many videos.
             The primary key from user will be a foreign key in video.
             */
-            User.hasMany(Video);
-            Video.belongsTo(User);
             mySQLDB.sync({
                 force: drop
             });
