927fb887a2c237772cb39e374ff3f7ee37757573
samuelyuoh <samuelyuoh@gmail.com>
2022-08-09 17:51:11 +0800

    ensure that log uuid is unique

--- helpers/logs.js
+++ helpers/logs.js
@@ -2,9 +2,13 @@ const { v4: uuidv4 } = require('uuid');
 const Logs = require('../models/Logs');
 function createlogs(action, userId) {
     uuid = uuidv4().toString().split('-')[0]
-    Logs.create({
-        logId: uuid, action: action, userId: userId
-    });
+    while (!Logs.findByPk(uuid)) {
+        Logs.create({
+            logId: uuid, action: action, userId: userId
+        });
+        uuid = uuidv4().toString().split('-')[0]
+    }
+    
 }
 
 module.exports = createlogs;
